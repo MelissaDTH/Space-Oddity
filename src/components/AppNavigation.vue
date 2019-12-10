7d653edb2523a083c335d66dd1d4555462adc2a6
MelissaDTH <melissa.hart@ziggo.nl>
2019-12-10 23:10:47 +0100

    formatted AppNav file

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,6 +1,13 @@
 <template>
   <span>
-    <v-navigation-drawer v-model="drawer" absolute temporary class="brown lighten-2" dark disable-resize-watcher>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+      class="grey"
+      dark
+      disable-resize-watcher
+    >
       <v-list>
         <template v-for="(item, index) in items">
           <v-list-item :key="index">
@@ -10,12 +17,13 @@
         </template>
       </v-list>
     </v-navigation-drawer>
+
     <v-toolbar color="blue darken-4" dark>
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="hidden-sm-and-up"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="hidden-sm-and-up"></v-app-bar-nav-icon>
       <v-spacer class="hidden-md-and-up"></v-spacer>
       <v-toolbar-title>{{appTitle}}</v-toolbar-title>
       <v-spacer class="hidden-sm-and-down"></v-spacer>
-      <v-btn text class="hidden-sm-and-down">PLANETS</v-btn>
+      <v-btn text class="hidden-sm-and-down">Planets</v-btn>
       <v-btn text class="hidden-sm-and-down">World map</v-btn>
       <v-btn text class="hidden-sm-and-down">Sign up</v-btn>
     </v-toolbar>
@@ -29,7 +37,11 @@ export default {
     return {
       appTitle: 'Space Oddity',
       drawer: false,
-      items: [{ title: 'Planets' }, { title: 'World map' }, { title: 'Sign up' }]
+      items: [
+        { title: 'Planets' },
+        { title: 'World map' },
+        { title: 'Sign up' }
+      ]
     };
   }
 };
