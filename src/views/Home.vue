b86ea2f282c4e4ab266dde29e91beb13ae2a4c72
MelissaDTH <melissa.hart@ziggo.nl>
2019-12-10 23:44:33 +0100

    Home page has content (lorem ipsum)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,20 @@
+<template>
+    <span>
+        <home-page />
+        <home-details />
+    </span>
+</template>
 
 <script>
 // @ is an alias to /src
+import HomePage from '@/components/HomePage';
+import HomeDetails from '@/components/HomeDetails'
+
+export default {
+    name: 'home',
+    components: {
+        HomePage,
+        HomeDetails
+    }
+};
 </script>
